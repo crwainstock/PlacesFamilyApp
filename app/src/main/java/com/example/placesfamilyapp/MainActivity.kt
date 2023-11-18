0e6e950976ad4e4c63dbf0790099703e4d3e03b7
Crystal <crwainstock@gmail.com>
2023-11-18 17:35:25 +0100

    added NearbyPlaceCard in UI, not complete

--- app/src/main/java/com/example/placesfamilyapp/MainActivity.kt
+++ app/src/main/java/com/example/placesfamilyapp/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.placesfamilyapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.placesfamilyapp.model.Place
 import com.example.placesfamilyapp.ui.theme.PlacesFamilyAppTheme
 
@@ -31,10 +35,23 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
 @Composable
 fun NearbyPlaceCard(place: Place, modifier: Modifier = Modifier) {
+    Card(modifier = modifier.padding(6.dp)){
+        Column {
+            Text (
+                text = place.name,
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.titleSmall
+            )
 
+        }
+    }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
