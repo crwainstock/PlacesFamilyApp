7cb6bbd35dafe6ca9754674f3d0e740549c70c11
Crystal <crwainstock@gmail.com>
2023-11-18 15:48:43 +0100

    add content to place.kt, not quite right yet

--- app/src/main/java/com/example/placesfamilyapp/model/Place.kt
+++ app/src/main/java/com/example/placesfamilyapp/model/Place.kt
@@ -1,4 +1,13 @@
 package com.example.placesfamilyapp.model
 
-class Place {
-}
+data class Place(
+    val name: String,
+    val address: String,
+    val types: List<String>,
+    // Add other properties as needed based on the actual JSON structure
+)
+
+data class PlacesApiResponse(
+    val results: List<Place>,
+    val status: String
+)
