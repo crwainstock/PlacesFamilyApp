289ed7966f7f11423f25a5cbfe9d60bd2ef76b48
Crystal <crwainstock@gmail.com>
2023-11-18 16:23:59 +0100

    add retrofit val in placesapiservice

--- app/src/main/java/com/example/placesfamilyapp/network/PlacesApiService.kt
+++ app/src/main/java/com/example/placesfamilyapp/network/PlacesApiService.kt
@@ -2,7 +2,10 @@ package com.example.placesfamilyapp.network
 
 
 import com.example.placesfamilyapp.model.PlacesApiResponse
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -21,3 +24,8 @@ interface PlacesApiService {
 private val json = Json {
     ignoreUnknownKeys = true
 }
+
+private val retrofit = Retrofit.Builder()
+    .baseUrl(BASE_URL)
+    .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+    .build()
