cf6e24baffe179a0d872fc913e1eada2cbe74568
Crystal <crwainstock@gmail.com>
2023-11-19 09:01:10 +0100

    update place.kt

--- app/src/main/java/com/example/placesfamilyapp/model/Place.kt
+++ app/src/main/java/com/example/placesfamilyapp/model/Place.kt
@@ -1,10 +1,11 @@
 package com.example.placesfamilyapp.model
 
+@Serializable
 data class Place(
     val name: String,
     val address: String,
     val types: List<String>,
-
+    //add access to img here later
 )
 
 data class PlacesApiResponse(
